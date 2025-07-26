f28e9f1132539154fe239aa24209a4c1ea3c0bc1
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-26 14:06:03 -0300

    ⚙️ config: setup server for agno playground

---
+++ src/playground.py
@@ -0,0 +1,36 @@
+from sys import path as sys_path
+from os import path as os_path
+
+from agno.playground import Playground
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from ai.news_workflow import NewsWorkflow
+from ai.agents import (
+    editor_agent,
+    scrapper_agent,
+    researcher_agent,
+    tagger_agent,
+    writer_agent,
+)
+
+sys_path.append(os_path.dirname(os_path.abspath(__file__)))
+
+
+playground = Playground(
+    name="News Workflow",
+    agents=[
+        editor_agent,
+        scrapper_agent,
+        researcher_agent,
+        tagger_agent,
+        writer_agent,
+    ],
+    teams=[NewsWorkflow().team],
+)
+
+app = playground.get_app()
+
+if __name__ == "__main__":
+    playground.serve(app="playground:app", reload=True)
