67673fe5a23bdae2f2dd3638945ae69647e9f06d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-26 14:07:12 -0300

    ⚙️ config: test NewsWorkflow execution

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from sys import path as sys_path
+from os import path as os_path
 
-app = FastAPI()
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from ai.news_workflow import NewsWorkflow
 
 
-@app.get("/")
-def read_root() -> dict[str, str]:
-    return {"message": "News AI is coming soon"}
+sys_path.append(os_path.dirname(os_path.abspath(__file__)))
+
+
+# from fastapi import FastAPI
+
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root() -> dict[str, str]:
+#     return {"message": "News AI is coming soon"}
+
+
+if __name__ == "__main__":
+    workflow = NewsWorkflow()
+    workflow.start()
