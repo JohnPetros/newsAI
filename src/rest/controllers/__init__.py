72978bc7733210c02ee672076e8e25395a9fa70a
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-31 20:06:39 -0300

    📶 rest: add CreatePostController

---
+++ src/rest/controllers/__init__.py
@@ -0,0 +1,3 @@
+from .create_post_controller import CreatePostController
+
+__all__ = ["CreatePostController"]
---
+++ src/rest/controllers/create_post_controller.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from ai import CreatePostWorkflow
+from rest.services import BlogService
+from entities import Post
+
+
+class Body(BaseModel):
+    category: str
+
+
+class CreatePostController:
+    def __init__(self, router: APIRouter) -> None:
+        @router.post("/post")
+        async def _(body: Body) -> Post:
+            workflow = CreatePostWorkflow()
+            post = workflow.start(body.category)
+
+            service = BlogService()
+            service.create_post(post)
+
+            return post
