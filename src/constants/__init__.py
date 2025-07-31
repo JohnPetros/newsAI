74188781b45abdf5a9484f09b924667daeded369
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-07-31 20:07:39 -0300

    🪨 constants: add Env

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+from .env import Env
+
+ENV = Env()
+
+__all__ = ["ENV"]
---
+++ src/constants/env.py
@@ -0,0 +1,9 @@
+from os import getenv
+from typing import NamedTuple
+
+
+class Env(NamedTuple):
+    host: str = str(getenv("HOST"))
+    port: int = int(getenv("PORT", 8000))
+    blog_api_url: str = str(getenv("BLOG_API_URL"))
+    google_api_key: str = str(getenv("GOOGLE_API_KEY"))
